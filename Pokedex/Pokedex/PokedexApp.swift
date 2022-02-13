0c3f3fd972b8643e8fc70903e5fc2de89d8c3dca
craig <craigbasketball@aol.com>
2022-02-13 11:21:17 -0800

    starter project code and added asset image

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Craig Belinfante on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
